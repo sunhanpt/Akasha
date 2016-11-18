d282899c4c6561f60d7353c7bca1bf595ae69b79
sunhanpt <763737898@qq.com>
2016-11-18 20:10:47 +0800

    D:wirteFile

--- Akasa/engine/akasa/main.cpp
+++ Akasa/engine/akasa/main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+#include <fstream>
 #include "AkaTypes.h"
 using namespace std;
 using namespace aka;
 
+class AWriteFile
+{
+public:
+	AWriteFile(const char * str);
+	~AWriteFile();
+	void Log(char * str);
+	void Flush();
+
+private:
+	ofstream * fout;
+};
+
+AWriteFile::AWriteFile(const char * str)
+{
+	fout = new ofstream(str, ios::app);
+}
+
+AWriteFile::~AWriteFile()
+{
+	fout->close();
+	delete fout;
+}
+
+void AWriteFile::Log(char * str)
+{
+	(*fout) << str;
+}
+
+void AWriteFile::Flush()
+{
+	fout->flush();
+}
+
 int main()
 {
-	s32 a = 0;
+	AWriteFile * writeFile = new AWriteFile("D:\\write.txt");
+	writeFile->Log("hahaha\n");
+	writeFile->Log("hahahahahaha\n");
+	writeFile->Log("hahahahahahahahaha\n");
+	delete writeFile;
+	return 0;
 }
