58376f073bed1d678b1f1e394159a824ae120d2f
sunhanpt <763737898@qq.com>
2018-10-28 22:14:38 +0800

    D: 添加RHIResource接口

--- Akasha/engine/include/RHIResources.h
+++ Akasha/engine/include/RHIResources.h
@@ -2,12 +2,14 @@
 #include "CoreDefines.h"
 #include "ThreadSafeCounter.h"
 #include <cassert>
+#include "ThreadSafeStack.h"
 
 class AKADLL_API ARHIResource
 {
 public:
-	ARHIResource()
+	ARHIResource(bool InbDoNotDeferDelte = false)
 		: m_MarkedForDelete(0)
+		, m_bDoNotDeferDelete(InbDoNotDeferDelte)
 		, m_bCommitted(false)
 	{
 	}
@@ -16,8 +18,48 @@ public:
 	{
 		assert(m_NumRefs.GetValue() == 0);
 	}
+
+	__forceinline uint32 AddRef() const 
+	{
+		int32 NewValue = m_NumRefs.Increment();
+		assert(NewValue > 0);
+		return uint32(NewValue);
+	}
+
+	__forceinline uint32 Release() const
+	{
+
+	}
+
+	__forceinline uint32 GetRefCount() const
+	{
+
+	}
+
+	void DoNoDeferDelete()
+	{
+
+	}
+
+	void SetCommitted(bool bInCommitted)
+	{
+
+	}
+
+	bool IsCommitted() const
+	{
+
+	}
+
+	static void FlushPendingDeltes();
+	static bool ByPass();
+
 private:
-	AThreadSafeCounter	m_NumRefs;
-	mutable	int32		m_MarkedForDelete;
-	bool				m_bCommitted;
+	mutable AThreadSafeCounter	m_NumRefs;
+	mutable	int32				m_MarkedForDelete;
+	bool						m_bDoNotDeferDelete;
+	bool						m_bCommitted;
+
+	static AThreadSafeStack<ARHIResource>	s_PendingDeletes;
+	static ARHIResource*					s_CurrentlyDeleting;
 };
