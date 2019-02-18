71e61ba2532df343452ed9548f88be4a97c6a11f
sunhanpt <763737898@qq.com>
2019-02-18 15:59:53 +0800

    D

--- Akasha/engine/include/GameInstance.h
+++ Akasha/engine/include/GameInstance.h
@@ -9,11 +9,17 @@ public:
 	virtual ~UGameInstance() {}
 
 public:
+	virtual void Init();
+
 	class UWorld* GetWorld() const;
+	struct FWorldContext* GetWorldContext() const { return WorldContext; };
 	class UEngine* GetEngine() const;
 	class UGameViewportClient* GetGameViewportClient() const;
 
 	virtual void StartGameInstance();
 	/** virtual function to allow custom GameInstances an opportunity to do cleanup when shutting down */
 	virtual void Shutdown();
+
+protected:
+	struct FWorldContext* WorldContext;
 };
